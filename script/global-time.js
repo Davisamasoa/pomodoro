dfbdce9293d3f6353246bb2adc396774a912c4e9
Davi Samuel <davisamasoa@gmail.com>
2022-09-12 11:04:31 -0300

    timezone finished

---
+++ script/global-time.js
@@ -0,0 +1,74 @@
+let br = document.querySelector(".brasil");
+let eua = document.querySelector(".eua");
+let uk = document.querySelector(".uk");
+let france = document.querySelector(".france");
+let japan = document.querySelector(".japan");
+let china = document.querySelector(".china");
+
+
+
+let date = new Date();
+let hours = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+
+let timeZoneEua = Number(hours) - 3;
+let timeZoneUk = Number(hours) + 4;
+let timeZoneFr = Number(hours) + 5;
+let timeZoneJapan = Number(hours) + 12;
+let timeZoneChina = Number(hours) + 11;
+
+function formatter (value) {
+    if (value < 10) {
+        value = "0" + value;
+    } else {
+        value = value;
+    }
+
+    return value;
+}
+
+
+time();
+
+setInterval(() => {
+    time();
+    
+}, 1000)
+
+
+function time() {
+    let date = new Date();
+    let denver = new Intl.DateTimeFormat('en-US', { timeZone: "America/Denver" }); 
+    let hours = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+
+
+    br.textContent = date.toLocaleTimeString("pt-BR");
+    eua.textContent = `${horas(timeZoneEua)}:${formatter(minute)}:${formatter(second)}`;
+    uk.textContent = `${horas(timeZoneUk)}:${formatter(minute)}:${formatter(second)}`;
+    france.textContent = `${horas(timeZoneFr)}:${formatter(minute)}:${formatter(second)}`;
+    japan.textContent = `${horas(timeZoneJapan)}:${formatter(minute)}:${formatter(second)}`;
+    china.textContent = `${horas(timeZoneChina)}:${formatter(minute)}:${formatter(second)}`;
+
+    if (second < 10) {
+        second = "0" + second;
+    }
+
+}
+
+
+
+function horas(value) {
+    if (value > 23) {
+        value = value - 24;
+
+    }
+    if (value < 10) {
+        value = "0" + value;
+    }
+    
+    return value;
+}
